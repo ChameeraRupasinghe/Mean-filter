02b61ad64c9106dad50070a722e74e4e0ad9cf40
ChameeraRupasinghe <chameera.15@cse.mrt.ac.lk>
2019-07-04 12:23:49 +0530

    Initail commit

---
+++ mean-filter.cu
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <iostream>
+#include <cuda.h>
+
+int main(int argc,char **argv)
+{
+   printf("Mean filter program\n");
+   FILE *fptr;
+   
+   printf("end\n");
+   return 0;
+}
